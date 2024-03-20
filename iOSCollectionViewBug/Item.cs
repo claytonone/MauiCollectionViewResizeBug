2de435d940128bea5a8be9aa66405550c51f86eb
Michael Clayton <michael.clayton@royalmail.com>
2024-03-20 15:36:45 +0000

    Initial commit

---
+++ iOSCollectionViewBug/Item.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace iOSCollectionViewBug
+{
+	public class Item : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _content;
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; OnPropertyChanged(); }
+        }
+
+        public Item()
+		{
+		}
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ iOSCollectionViewBug/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace iOSCollectionViewBug;
+
+public partial class MainPage : ContentPage
+{
+    private List<Item> _items;
+	public List<Item> Items
+	{
+		get { return _items; }
+		set { _items = value; OnPropertyChanged(); }
+	}
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+        BindingContext = this;
+
+		Items = new List<Item>()
+		{
+            new Item() { Content = "Some text" },
+            new Item() { Content = "Some text" },
+            new Item() { Content = "Some text" },
+            new Item() { Content = "Some text" },
+            new Item() { Content = "Some text" },
+            new Item() { Content = "Some text" },
+            new Item() { Content = "Some text" },
+            new Item() { Content = "Some text" },
+            new Item() { Content = "Some text" },
+            new Item() { Content = "Some text" },
+            new Item() { Content = "Some text" },
+            new Item() { Content = "Some text" }
+        };
+    }
+
+    void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+        Items[0].Content = "Some much much much much much much much much much much much much much much much much longer text";
+        Items[2].Content = "Some much much much much much much much much much much much much much much much much longer text";
+        Items[4].Content = "Some much much much much much much much much much much much much much much much much longer text";
+    }
+}
